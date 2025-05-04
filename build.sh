de759e18804454e7a089ec221b89c81bee6278fc
Sarthak Sharma <SharmaSarthak999@gmail.com>
2025-05-04 19:28:19 +0530

    Initialized fedsim-server

--- build.sh
+++ build.sh
@@ -7,4 +7,11 @@ echo "***************************************************************"
 podman build -f ./control-plane/Dockerfile -t control-plane:latest ./control-plane/
 echo "control-plane image built successfully"
 
+echo "***************************************************************"
+echo "Building fedsim-server image"
+echo "***************************************************************"
+
+podman build -f ./fedsim-server/Dockerfile -t fedsim-server:latest ./fedsim-server/
+echo "fedsim-server image built successfully"
+
 
