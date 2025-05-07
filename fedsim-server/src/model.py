16270945a8d9ac0ca6f32884a5451f12fe4e82f4
Sarthak Sharma <SharmaSarthak999@gmail.com>
2025-05-07 22:13:21 +0530

    Added multiple function in FLModel Added NumpyModel

--- fedsim-server/src/model.py
+++ fedsim-server/src/model.py
@@ -1,6 +1,6 @@
 import torch
 import numpy as np
-
+import operator
 
 class FLModel(torch.nn.Module):
     def __init__(self, device):
@@ -34,6 +34,110 @@ class FLModel(torch.nn.Module):
 
         return loss.item(), acc
 
+    def get_params(self):
+        ps = []
+        for p in list(self.parameters()):
+            ps.append(
+                np.copy(
+                    p.data.cpu().numpy()
+                )
+            )
+        for bn in self.bn_layers:
+            ps.append(
+                np.copy(
+                    bn.running_mean.cpu().numpy()
+                )
+            )
+            ps.append(
+                np.copy(
+                    bn.running_var.cpu().numpy()
+                )
+            )
+        NumpyModel(ps)
+
+    def get_bn_values(self, setting=0):
+        """{0: usyb, 1: yb, 2: us, 3: none}"""
+        if setting not in [0, 1, 2, 3]:
+            raise ValueError("Setting must be in 0, 1, 2, 3")
+
+        vals = []
+
+        if setting == 3:
+            return vals
+
+        with torch.no_grad():
+            if setting in [0,1]:
+                for bn in self.bn_layers:
+                    vals.append(
+                        np.copy(
+                            bn.weight.cpu().numpy()
+                        )
+                    )
+                    vals.append(
+                        np.copy(
+                            bn.bias.cpu().numpy()
+                        )
+                    )
+            if setting in [0,2]:
+                for bn in self.bn_layers:
+                    vals.append(
+                        np.copy(
+                            bn.running_mean.cpu().numpy()
+                        )
+                    )
+                    vals.append(
+                        np.copy(
+                            bn.running_var.cpu().numpy()
+                        )
+                    )
+        return vals
+
+    def set_bn_values(self, vals, setting=0):
+        """{0: usyb, 1: yb, 2: us, 3: none}"""
+        if setting not in [0, 1, 2, 3]:
+            raise ValueError("Setting must be in 0, 1, 2, 3")
+
+        if setting == 3:
+            return
+
+        with torch.no_grad():
+            i=0
+            if setting in [0,1]:
+                for bn in self.bn_layers:
+                    bn.weight.copy_(torch.tensor(vals[i]))
+                    bn.bias.copy_(torch.tensor(vals[i+1]))
+                    i+=2
+            if setting in [0,2]:
+                for bn in self.bn_layers:
+                    bn.running_mean.copy_(torch.tensor(vals[i]))
+                    bn.running_var.copy_(torch.tensor(vals[i+1]))
+                    i+=2
+
+    def set_params(self, params):
+        i =0
+        with torch.no_grad():
+            for p in self.parameters():
+                p.copy_(torch.tensor(params[i]))
+                i+=1
+            for bn in self.bn_layers:
+                bn.running_mean.copy_(torch.tensor(vals[i]))
+                bn.running_var.copy_(torch.tensor(vals[i+1]))
+                i+=2
+
+    def test(self, x, y, B):
+        self.eval()
+        n_batches = int(np.ceil(x.shape[0]/B))
+        loss = 0.0
+        acc = 0.0
+
+        with torch.no_grad():
+            for b in range(n_batches):
+                logits = self.forward(x[b*B:(b+1)*B])
+                loss += self.loss_fn(logits, y[b*B:(b+1)*B]).item()
+                acc += self.calc_acc(logits, y[b*B:(b+1)*B])
+        self.train()
+        return loss/n_batches, acc/n_batches
+
 class MNISTModel(FLModel):
     def __init__(self, device):
         super(MNISTModel, self).__init__(device)
@@ -56,3 +160,61 @@ class MNISTModel(FLModel):
 
     def calc_acc(self, logits, y):
         return (torch.argmax(logits, dim=1) == y).float().mean()
+
+class NumpyModel():
+    
+    def __init__(self, params):
+        self.params = params
+        
+    def copy(self):
+        return NumpyModel([np.copy(p) for p in self.params])
+        
+    def zeros_like(self):
+        return NumpyModel([np.zeros_like(p) for p in self.params])
+        
+    def _op(self, other, f):
+        if np.isscalar(other):
+            new_params = [f(p, other) for p in self.params]
+            
+        elif isinstance(other, NumpyModel):
+            new_params = [f(p, o) for (p, o) in zip(self.params, other.params)]
+            
+        else:
+            raise ValueError('Incompatible type for op: {}'.format(other))
+        
+        return NumpyModel(new_params)
+        
+        
+    def abs(self):
+        return NumpyModel([np.absolute(p) for p in self.params])
+        
+    def __add__(self, other):
+        return self._op(other, operator.add)
+        
+    def __radd__(self, other):
+        return self._op(other, operator.add)
+
+    def __sub__(self, other):
+        return self._op(other, operator.sub)
+        
+    def __mul__(self, other):
+        return self._op(other, operator.mul)
+        
+    def __rmul__(self, other):
+        return self._op(other, operator.mul)
+        
+    def __truediv__(self, other):
+        return self._op(other, operator.truediv)
+        
+    def __pow__(self, other):
+        return self._op(other, operator.pow)
+        
+    def __getitem__(self, key):
+        return self.params[key]
+        
+    def __len__(self):
+        return len(self.params)
+        
+    def __iter__(self):
+        for p in self.params:
+            yield p
