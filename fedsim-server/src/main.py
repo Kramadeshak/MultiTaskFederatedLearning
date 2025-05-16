54b0aff7dabc8ba6b1ded89358c880a9ef16562e
Sarthak Sharma <SharmaSarthak999@gmail.com>
2025-05-16 21:48:12 +0530

    Introuced multi-threaded handling of requests in fedsim-server

--- fedsim-server/src/main.py
+++ fedsim-server/src/main.py
@@ -1,39 +1,39 @@
 import socket
 import pickle
 import torch
+import threading
 from model import MNISTModel
 
 HOST = '0.0.0.0'  # Listen on all interfaces
 PORT = 8080       # Port to listen on
 
-def main():
+def handle_client_connection(conn, addr, serialized_params):
+    print(f"[INFO] Connected by {addr}")
+    conn.sendall(len(serialized_params).to_bytes(8, byteorder='big'))
+    print(f"sent {len(serialized_params)}")
+    conn.sendall(serialized_params)
+    print("[INFO] Parameters sent.")
+
+def get_model_params():
     device = torch.device('cpu')  # Send CPU tensors only
     model = MNISTModel(device)
-
-    # Example: initialize optimizer to make model "complete"
     model.set_optim(torch.optim.SGD(model.parameters(), lr=0.01))
-
-    # Get model parameters
     model_params = model.get_params()
-
-    # Serialize model parameters with pickle
     serialized_params = pickle.dumps(model_params)
+    return serialized_params
 
+def main():
+
+    params = get_model_params()
     print(f"[INFO] Waiting for connection on port {PORT}...")
 
-    # Start TCP server
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen(1)
-        conn, addr = s.accept()
-        with conn:
-            print(f"[INFO] Connected by {addr}")
-            # First, send the length of the payload
-            conn.sendall(len(serialized_params).to_bytes(8, byteorder='big'))
-            print(f"sent {len(serialized_params)}")
-            # Then, send the actual payload
-            conn.sendall(serialized_params)
-            print("[INFO] Parameters sent.")
+        while True:
+            conn, addr = s.accept()
+            thread = threading.Thread(target=handle_client_connection, args=(conn, addr, params))
+            thread.start()
 
 if __name__ == "__main__":
     main()
